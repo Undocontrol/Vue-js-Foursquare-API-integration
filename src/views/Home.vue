4d726e8aceac224b3e1892140b3989452eb1b128
Emma <Undocontrol88@gmail.com>
2019-03-25 14:49:08 +0000

    Imports axis and adds Home view-

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<!--
+File Overview:
+* This is the main "view", and importing all components into this view.
+-->
+
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped>
+
+</style>
